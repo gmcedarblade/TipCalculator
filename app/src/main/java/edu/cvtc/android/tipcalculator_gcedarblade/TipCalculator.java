cdbbbb914a078d24d37db583d9e7e996592ea326
Greg Cedarblade <gmcedarblade@gmail.com>
2017-04-16 21:56:12 -0500

    added more funcationality to tip calc

--- app/src/main/java/edu/cvtc/android/tipcalculator_gcedarblade/TipCalculator.java
+++ app/src/main/java/edu/cvtc/android/tipcalculator_gcedarblade/TipCalculator.java
@@ -1,15 +1,20 @@
 package edu.cvtc.android.tipcalculator_gcedarblade;
 
 import android.annotation.TargetApi;
+import android.content.SharedPreferences;
 import android.icu.text.NumberFormat;
 import android.os.Build;
+import android.support.v4.app.SharedElementCallback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -19,11 +24,28 @@ public class TipCalculator extends AppCompatActivity implements TextView.OnEdito
     // widgets
     private EditText billAmountEditText;
     private TextView tipPercentTextView;
+
     private Button tipPercentUpButton;
     private Button tipPercentDownButton;
+
     private TextView tipAmountTextView;
     private TextView totalAmountTextView;
 
+    private RadioButton roundNoneRadioButton;
+    private RadioButton roundTipRadioButton;
+    private RadioButton roundTotalRadioButton;
+
+    private Spinner splitSpinner;
+
+    private TextView perPersonLabelTextView;
+    private TextView perPersonAmountLabelTextView;
+
+    private Button applyButton;
+
+    // shared preferences
+    private SharedPreferences savedValues;
+
+    // instance variables
     private String billAmountString = "";
     private float tipPercent = .15f;
 
@@ -42,11 +64,60 @@ public class TipCalculator extends AppCompatActivity implements TextView.OnEdito
         tipAmountTextView = (TextView) findViewById(R.id.tipAmountTextView);
         totalAmountTextView = (TextView) findViewById(R.id.totalAmountTextView);
 
+        roundNoneRadioButton = (RadioButton) findViewById(R.id.noneRadioButton);
+        roundTipRadioButton = (RadioButton) findViewById(R.id.tipRadioButton);
+        roundTotalRadioButton = (RadioButton) findViewById(R.id.totalRadioButton);
+
+        splitSpinner = (Spinner) findViewById(R.id.splitBillSpinner);
+
+        perPersonLabelTextView = (TextView) findViewById(R.id.perPersonLabelTextView);
+        perPersonAmountLabelTextView = (TextView) findViewById(R.id.perPersonAmountLabelTextView);
+
+        applyButton = (Button) findViewById(R.id.applyButton);
+
+        // array adapter for spinner
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.split_array, android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        splitSpinner.setAdapter(adapter);
+
         // Listeners
         billAmountEditText.setOnEditorActionListener(this);
         tipPercentUpButton.setOnClickListener(this);
         tipPercentDownButton.setOnClickListener(this);
+        applyButton.setOnClickListener(this);
 
+        // get SharedPerferences object
+        savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
+
+    }
+
+    @Override
+    public void onPause() {
+        // save the instance variables
+        SharedPreferences.Editor editor = savedValues.edit();
+        editor.putString("billAmountString", billAmountString);
+        editor.putFloat("tipPercent", tipPercent);
+        editor.apply();
+
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // get the instance variables
+        billAmountString = savedValues.getString("billAmountString", "");
+        tipPercent = savedValues.getFloat("tipPercent", 0.15f);
+
+        // set the bill amount on its widget
+        billAmountEditText.setText(billAmountString);
+
+        // calculate and display
+        calculateAndDisplay();
     }
 
     @TargetApi(Build.VERSION_CODES.N)
@@ -62,13 +133,39 @@ public class TipCalculator extends AppCompatActivity implements TextView.OnEdito
         }
 
         // calculate tip and total amount
-        float tipAmount = billAmount * tipPercent;
-        float totalAmount = billAmount + tipAmount;
+        float tipAmount = 0;
+        float totalAmount = 0;
+
+        if (roundNoneRadioButton.isChecked()) {
+            tipAmount = billAmount * tipPercent;
+            totalAmount = billAmount + tipAmount;
+        } else if (roundTipRadioButton.isChecked()) {
+            tipAmount = StrictMath.round(billAmount * tipPercent);
+            totalAmount = billAmount + tipAmount;
+        } else if (roundTotalRadioButton.isChecked()) {
+            float tipNotRounded = billAmount * tipPercent;
+            totalAmount = StrictMath.round(billAmount + tipNotRounded);
+            tipAmount = totalAmount - billAmount;
+        }
+
+        // split amount and show/hide split amount widgets
+        int splitPosition = splitSpinner.getSelectedItemPosition();
+        int split = splitPosition + 1;
+        float perPersonAmount = 0;
+        if (split == 1) {  // no split - hide widgets
+            perPersonLabelTextView.setVisibility(View.GONE);
+            perPersonAmountLabelTextView.setVisibility(View.GONE);
+        } else { // split - show widgets
+            perPersonAmount = totalAmount / split;
+            perPersonLabelTextView.setVisibility(View.VISIBLE);
+            perPersonAmountLabelTextView.setVisibility(View.VISIBLE);
+        }
 
         // display results to user
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         tipAmountTextView.setText(currency.format(tipAmount));
         totalAmountTextView.setText(currency.format(totalAmount));
+        perPersonAmountLabelTextView.setText(currency.format(perPersonAmount));
 
         NumberFormat percent  = NumberFormat.getPercentInstance();
         tipPercentTextView.setText(percent.format(tipPercent));
